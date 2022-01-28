82c5b30eb3e869d7b292f8972604f9266b579fcf
Glitch (large-bustling-trade) <none>
2022-01-28 12:25:11 +0000

    🕦☺️ Updated with Glitch

---
+++ commands/moderator.js
@@ -0,0 +1,14 @@
+{
+  const Server = require("/server.js");
+  var groupid = "GROUP ID";
+  const ownersendinggroupwall = "!moderator-aç";
+  const sendgroupwall = "Moderator Başarılı Şeklinde Açıldı";
+  const blockscamlinks = true;
+  const blockspamwords = true;
+  const blockbadwords = true;
+  const rankrequirement = false;
+  const badwords =
+    "s*a*, sca*, scam, sc*m, f*k*, fak*, fa*e, fake, r*b*x, ro*u*, rob*x, *o*u*";
+  var BADWORDS =
+    "s*a*, sca*, scam, sc*m, f*k*, fak*, fake, r*b*u, ro*u*, rob*x, *o*u*";
+}
